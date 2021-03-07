2071e1674d4c675e5278a38e86e230e8c9fad21b
nakai kensuke <nakaikensuke@nakainoMacBook-Air.local>
2021-03-07 18:07:48 +0900

    add rspec

--- spec/pharaoh_lang_spec.rb
+++ spec/pharaoh_lang_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe PharaohLang do
     expect(PharaohLang::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "to_hieroglyphがローマ字に対して呼び出された時" do
+    it "ヒエログリフに変換される" do
+      string = "koncha Pharaoh"
+      expect(string.to_hieroglyph).to eql("𓎡𓍯𓈖𓎡𓎛𓄿 𓏤𓎛𓄿𓂋𓄿𓍯𓎛")
+    end
+  end
+  context "to_hieroglyphがローマ字以外に対して呼び出された時" do
+    it "そのままの文字が出力される" do
+      string = "ファラオ こんちゃ!"
+      expect(string.to_hieroglyph).to eql("ファラオ こんちゃ!")
+    end
   end
 end
